95f7e11cbef5760f8844dd2ddf09e9cedc530ff5
yemashev99 <yemashev99@mail.ru>
2020-03-21 18:59:24 +0700

    stage

--- frontend/views/cabinet/index.php
+++ frontend/views/cabinet/index.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = 'Личный кабинет';
     <label for="tab1">Ваша корзина</label>
 
     <input id="tab2" type="radio" name="tabs" <?php if($cabinet == 'orders') : ?> checked <?php endif; ?>>
-    <label for="tab2">Все заказы</label>
+    <label for="tab2">Заказы</label>
 
     <input id="tab3" type="radio" name="tabs">
     <label for="tab3">Контактные данные</label>
@@ -154,6 +154,88 @@ $this->params['breadcrumbs'][] = 'Личный кабинет';
 
     <section id="content2">
         <div class="my-tabs-tab">
+            <div class="form_title">
+                <h3>Заказы в обработке</h3>
+            </div>
+            <div class="bx-basket bx-blue bx-step-opacity" style="opacity: 1;">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="basket-items-list-wrapper basket-items-list-wrapper-height-fixed">
+                            <div class="basket-items-list-container">
+                                <table class="basket-items-list-table">
+                                    <tbody>
+                                    <?php foreach ($cartItems as $cartItem) : ?>
+                                        <tr class="basket-items-list-item-container">
+                                            <td class="basket-items-list-item-descriptions">
+                                                <div class="basket-items-list-item-descriptions-inner">
+                                                    <div class="basket-item-block-image">
+                                                        <a href="" class="basket-item-image-link">
+                                                            <?=Html::img(Yii::getAlias('@web').'/'.$cartItem->product->image, [
+                                                                'class' => 'basket-item-image',
+                                                                'alt' => $cartItem->product->title
+                                                            ])?>
+                                                        </a>
+                                                    </div>
+                                                    <div class="basket-item-block-info">
+                                                        <span class="basket-item-actions-remove visible-xs"></span>
+                                                        <h2 class="basket-item-info-name">
+                                                            <?=Html::a('<span>'.$cartItem->product->title.'</span>', '#', ['class' => 'basket-item-info-name-link'])?>
+                                                        </h2>
+                                                        <div class="basket-item-block-comment">
+                                                            <div class="basket-item-property-custom-name">
+                                                                Комментарий к заказу
+                                                            </div>
+
+                                                            <textarea class="comment" id="<?=$cartItem->product_id?>" style="width: 90%; min-height: 65px;"><?=$cartItem->comment?></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="basket-items-list-item-price basket-items-list-item-price-for-one hidden-xs">
+                                                <div class="basket-item-block-price">
+                                                    <div class="basket-item-price-current">
+                                                            <span class="basket-item-price-current-text">
+                                                                <?=$cartItem->product->price?> ₽
+                                                            </span>
+                                                    </div>
+                                                    <div class="basket-item-price-title">
+                                                        цена за 1 <?=$cartItem->product->measure?>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="basket-items-list-item-amount">
+                                                <div class="basket-item-block-amount">
+                                                    <?=Html::a('', ['cabinet/down', 'product_id' => $cartItem->product_id], ['class' => 'basket-item-amount-btn-minus minus', 'id' => $cartItem->product_id])?>
+                                                    <div class="basket-item-amount-filed-block">
+                                                        <?=Html::input('text', 'quantity', $cartItem->quantity, [
+                                                            'class' => 'basket-item-amount-filed',
+                                                            'readonly' => 'readonly',
+                                                        ])?>
+                                                    </div>
+                                                    <?=Html::a('', ['cabinet/up', 'product_id' => $cartItem->product_id], ['class' => 'basket-item-amount-btn-plus plus', 'id' => $cartItem->product_id])?>
+                                                    <div class="basket-item-amount-field-description">
+                                                        <?=$cartItem->product->measure?>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="basket-items-list-item-price">
+                                                <div class="basket-item-block-price">
+                                                    <div class="basket-item-price-current">
+                                                            <span class="basket-item-price-current-text">
+                                                               <?=Cart::totalProduct($cartItem->quantity, $cartItem->product->price)?> ₽
+                                                            </span>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="form_title">
                 <h3>Все заказы</h3>
             </div>
